036090933da223efccaa7cb844a1d30fcef5ae04
Mitchell <nowheredust@gmail.com>
2019-04-30 21:43:53 -0400

    Added project

---
+++ aMAZEing/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace aMAZEing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateMazeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generateMazeBtn
+            // 
+            this.generateMazeBtn.Location = new System.Drawing.Point(13, 523);
+            this.generateMazeBtn.Name = "generateMazeBtn";
+            this.generateMazeBtn.Size = new System.Drawing.Size(161, 76);
+            this.generateMazeBtn.TabIndex = 1;
+            this.generateMazeBtn.Text = "Generate a Maze";
+            this.generateMazeBtn.UseVisualStyleBackColor = true;
+            this.generateMazeBtn.Click += new System.EventHandler(this.generateMazeBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 611);
+            this.Controls.Add(this.generateMazeBtn);
+            this.Name = "MainForm";
+            this.Text = "aMAZEing";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button generateMazeBtn;
+    }
+}
+
---
+++ aMAZEing/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aMAZEing
+{
+    public partial class MainForm : Form
+    {
+        private const int MAZE_FACTOR = 1;
+        private const int UPPER_BUFFER = 20;
+        private const int LOWER_BUFFER = 150;
+        private const int SIDE_BUFFER = 20;
+        private const int RIGHT_BUFFER = SIDE_BUFFER * 2;
+        private int[,] mazeLayout;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void generateMazeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+            //The width and height count is off by 2. Not sure why
+            int cellsWidth = ((this.Width - RIGHT_BUFFER) / MAZE_FACTOR) + 2;
+            int cellsHeight = ((this.Height - LOWER_BUFFER) / MAZE_FACTOR) + 2;
+            mazeLayout = new int[cellsHeight, cellsWidth];
+
+            for(int widthLine = 1; widthLine < cellsHeight; widthLine++)
+            {
+                g.DrawLine(p, SIDE_BUFFER, widthLine * UPPER_BUFFER, this.Width - RIGHT_BUFFER + 4, widthLine * UPPER_BUFFER); //Not sure why but the width is off by 4, this corrects
+            }
+            for(int heightLine = 1; heightLine < cellsWidth; heightLine++)
+            {
+                g.DrawLine(p, (heightLine * SIDE_BUFFER), UPPER_BUFFER, (heightLine * SIDE_BUFFER), this.Height - LOWER_BUFFER);
+            }
+        }
+    }
+}
